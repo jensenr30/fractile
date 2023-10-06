1696ce127f1f62539c450acfa427b4f31d657814
Ryan Jensen <jensenr30@gmail.com>
2023-10-06 02:20:42 -0500

    delete commented-out code

--- src/graphics.c
+++ src/graphics.c
@@ -153,41 +153,6 @@ short draw_rect(
     return 0;
 }
 
-/*
-/// returns a pointer to the loaded texture.
-SDL_Texture *load_image_to_texture(char *filename){
-
-        //attempt to load the image
-        SDL_Surface *tempSurface = IMG_Load(filename);
-
-        // if the image was not loaded to a surface correctly,
-        if(tempSurface == NULL){
-                // create an error message to write to file.
-                char message[512];
-                strcpy(message, "error in load_image(): could not load file to surface \"");
-                strcat(message, filename);
-                strcat(message, "\"");
-                error(message);
-                // and return NULL.
-                return NULL;
-        }
-
-        // try to convert the surface into a texture
-        SDL_Texture *tempTexture = SDL_CreateTextureFromSurface(myRenderer, tempSurface);
-
-        // if the texture was not created from surface correctly,
-        if(tempTexture == NULL){
-                // write and error message to the error log
-                error("error in load_image(): could not create texture from surface");
-                // and return NULL.
-                return NULL;
-        }
-
-        //  if the image loaded correctly, return a pointer to the texture that contains it.
-        return tempTexture;
-}
-*/
-
 /// intersect_p is for when you know some point on each line <x,y> and the slopes of each line.
 // returns 1 (1)	 if there is a single intersection
 // returns 0 (0) if the lines are parallel but they are not the same line
